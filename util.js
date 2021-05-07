b090336dd8118c29399e9474ab38d1a5b5f1b19a
Tirth Patel <tirrrthpatel@gmail.com>
2021-05-07 18:59:26 +0530

    Add utility functions

---
+++ util.js
@@ -0,0 +1,98 @@
+var http = require("http");
+const crypto = require("crypto");
+const variationsStream = require("variations-stream");
+const pkg = require("./package.json");
+
+export const jwtCracker = () => {
+  const defaultAlphabet =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const defaultMaxLength = 12;
+  const defaultToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+  const token = process.env.TOKEN || defaultToken;
+  const alphabet = process.env.ALPHABET || defaultAlphabet;
+  const maxLength = Number(process.env.LENGTH) || defaultMaxLength;
+
+  if (typeof token === "undefined" || token === "--help") {
+    console.log(
+      `jwt-cracker version ${pkg.version}
+  Usage:
+    jwt-cracker <token> [<alphabet>] [<maxLength>]
+    token       the full HS256 jwt token to crack
+    alphabet    the alphabet to use for the brute force (default: ${defaultAlphabet})
+    maxLength   the max length of the string generated during the brute force (default: ${defaultMaxLength})
+`
+    );
+    process.exit(0);
+  }
+
+  const generateSignature = function (content, secret) {
+    return crypto
+      .createHmac("sha256", secret)
+      .update(content)
+      .digest("base64")
+      .replace(/=/g, "")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_");
+  };
+
+  const printResult = function (startTime, attempts, result) {
+    if (result) {
+      console.log("SECRET FOUND:", result);
+    } else {
+      console.log("SECRET NOT FOUND");
+    }
+    console.log("Time taken (sec):", (new Date().getTime() - startTime) / 1000);
+    console.log("Total Attempts:", attempts);
+  };
+
+  const [header, payload, signature] = token.split(".");
+  const content = `${header}.${payload}`;
+
+  const startTime = new Date().getTime();
+  let attempts = 0;
+
+  variationsStream(alphabet, maxLength)
+    .on("data", function (comb) {
+      attempts++;
+      const currentSignature = generateSignature(content, comb);
+      if (attempts % 100000 === 0) {
+        console.log("Attempts:", attempts);
+      }
+      if (currentSignature == signature) {
+        setInterval(() => {
+          printResult(startTime, attempts, comb);
+        }, 2500);
+        // process.exit(0);
+      }
+    })
+    .on("end", function () {
+      setInterval(() => {
+        printResult(startTime, attempts);
+      }, 2500);
+      // process.exit(1);
+    });
+};
+
+export function startKeepAlive() {
+  setInterval(function () {
+    var options = {
+      host: "jwt-cracker.herokuapp.com",
+      // port: 80,
+      path: "/",
+    };
+    http
+      .get(options, function (res) {
+        res.on("data", function (chunk) {
+          try {
+            // optional logging... disable after it's working console.log("HEROKU RESPONSE: " + chunk);
+          } catch (err) {
+            console.log(err.message);
+          }
+        });
+      })
+      .on("error", function (err) {
+        console.log("Error: " + err.message);
+      });
+  }, 20 * 60 * 1000); // load every 20 minutes
+}
